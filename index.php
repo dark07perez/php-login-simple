38c4dcfddbc05a9521e36cad28fb016074711e0e
SergioPerez <dark07perez@gmail.com>
2018-08-11 05:54:46 -0500

    users por USERS en BD

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   require 'controller/database.php';
 
   if (isset($_SESSION['user_id'])) {
-    $records = $conn->prepare('SELECT id, email, password FROM users WHERE id = :id');
+    $records = $conn->prepare('SELECT id, email, password FROM USERS WHERE id = :id');
     $records->bindParam(':id', $_SESSION['user_id']);
     $records->execute();
     $results = $records->fetch(PDO::FETCH_ASSOC);
--- pages/signup.php
+++ pages/signup.php
@@ -5,7 +5,7 @@
   $message = '';
 
   if (!empty($_POST['email']) && !empty($_POST['password'])) {
-    $sql = "INSERT INTO users (email, password) VALUES (:email, :password)";
+    $sql = "INSERT INTO USERS (email, password) VALUES (:email, :password)";
     $stmt = $conn->prepare($sql);
     $stmt->bindParam(':email', $_POST['email']);
     $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
