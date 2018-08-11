ca6ada576542324103624e62b3c2d7cbb1b065f2
SergioPerez <dark07perez@gmail.com>
2018-08-11 05:36:21 -0500

    ajustes conecxion BD

--- controller/database.php
+++ controller/database.php
@@ -5,10 +5,14 @@ $username = 'root';
 $password = 'admin';
 $database = 'php_login_database';
 
+$conn = mysqli_connect($server, $username,$password,$database) or die('Error al conectar con Mysql Server.');
+
+/* 
 try {
   $conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
 } catch (PDOException $e) {
   die('Connection Failed: ' . $e->getMessage());
 }
+*/
 
 ?>
