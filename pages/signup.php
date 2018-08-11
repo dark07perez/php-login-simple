77a1bc3dce641d6416565ff24d26a83220cf65fd
SergioPerez <dark07perez@gmail.com>
2018-08-11 11:11:25 -0500

    creacion terminado

--- pages/signup.php
+++ pages/signup.php
@@ -1,20 +1,20 @@
 <?php
-
+  /*
   require '../controller/database.php';
 
   $message = '';
 
   if (!empty($_POST['email']) && !empty($_POST['password'])) {  
     
-    #$sql = "INSERT INTO USERS (email, password) VALUES (:email, :password)";
-    #$stmt = $conn->prepare($sql);
-    #$stmt->bindParam(':email', $_POST['email']);
+    $sql = "INSERT INTO USERS (email, password) VALUES (:email, :password)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':email', $_POST['email']);
     $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
-    #$stmt->bindParam(':password', $password);
+    $stmt->bindParam(':password', $password);
 
     $query = "INSERT INTO USERS (email, password) VALUES (:email, :password)";
     $consul = mysqli_query($conn, $query);
-    #$results = mysqli_fetch_array($consul);
+    $results = mysqli_fetch_array($consul);
 
     if ($stmt->execute()) {
       $message = 'Successfully created new user';
@@ -22,6 +22,44 @@
       $message = 'Sorry there must have been an issue creating your account';
     }
   }
+  
+  */
+
+  require '../controller/database.php';
+
+  $message = '';
+
+  if (!empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['confirm_password'])) {  
+
+    if($_POST['password'] == $_POST['confirm_password']){
+
+      $query = "SELECT id FROM USERS WHERE email ='".$_POST['email']."'";
+      $consul = mysqli_query($conn, $query);
+      $results = mysqli_fetch_array($consul);
+      
+      if (count($results) == 0) {
+
+        $query = "INSERT INTO USERS (email, password) VALUES ('".$_POST['email']."',".$_POST['confirm_password']."')";
+        $consul = mysqli_query($conn, $query);
+        
+        $message = 'Successfully created new user';
+
+      }
+      else{
+        $message = 'El email ya existe';
+      }
+
+    }else{
+      $message="La contraseña no coincide";
+    }
+    
+  }
+  else{
+    $message="Hay campos vacios, llene los campos";
+  }
+
+
+  
 ?>
 <!DOCTYPE html>
 <html>
